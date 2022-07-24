494fcc04fbf7a7f97f831607b2b1f3fdcc615fef
alocri <aloecristian@yahoo.com>
2022-07-24 18:16:50 +0200

    16. Introduzione alla funzione di routing dell'Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alphashop';
-  saluti: string = "Benvenuti in alphashop";
-  bollini: number = 1500;
+
 }
